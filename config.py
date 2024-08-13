4babe8feb5b49ce467c36ab7dd8e30923a278d03
Yutabhai <155735358+Yutabhai@users.noreply.github.com>
2024-08-13 21:21:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,18 +7,18 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "23902408")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "6a36a4ef2f07d63aeba7b53b99c64d73") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
-    FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7354411402:AAFM_WEoZsNNUDP_Xs4hngtJmkC1PYaYMgk") # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', '-1002044619047') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
     # database config
-    DB_URL  = os.environ.get("DB_URL", "")  # ⚠️ Required
-    DB_NAME  = os.environ.get("DB_NAME","SnowEncoderBot") 
+    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://mex:rex@cluster7.jrzowqz.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
+    DB_NAME  = os.environ.get("DB_NAME","ZoroEncBot") 
 
     # Other Configs 
-    ADMIN = int(os.environ.get("ADMIN", "0")) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '0')) # ⚠️ Required
+    ADMIN = int(os.environ.get("ADMIN", "7179837246")) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002223844205')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
     START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
 
